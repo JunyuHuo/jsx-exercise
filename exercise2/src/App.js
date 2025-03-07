48c95df182c98b4693a7e166aa83e113135ac0a9
JunyuHuo <junyu.huo@ucalgary.ca>
2025-03-07 12:02:12 -0700

    finish

--- exercise2/src/App.js
+++ exercise2/src/App.js
@@ -5,17 +5,10 @@ import Contact from './Contact';
 
 function App() {
 
-  const currentYear = new Date().getFullYear();
-  
-
   const isLoggedIn = true; 
   
   return (
     <div className="App">
-      <h1>ENSF-381: Full Stack Web Development</h1>
-      <p>React Components</p>
-      <p>Current Year: {currentYear}</p>
-      <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
       
       <Home 
         title="Home Page" 
